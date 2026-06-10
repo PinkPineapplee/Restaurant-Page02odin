e4d1fafb2eb946bd79d06be8c94604797a0506dc
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2026-06-10 12:55:45 +0100

    load css file in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,12 @@ export default {
       template: "./src/index.html",
     }),
   ],
-
-
-}; 
+ module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
