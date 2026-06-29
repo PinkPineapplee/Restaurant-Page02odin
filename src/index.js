85a3d5416179c37effb08819b7cf57756e7d54fb
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2026-06-29 15:17:56 +0100

    fix image error

--- src/index.js
+++ src/index.js
@@ -3,5 +3,11 @@
  // index.js
 import "./styles.css";
 import { greeting } from "./greeting.js";
+import baconChaddar from "src/images/Bacon_Cheddar_Double.jpg";
+
+const image = document.createElement("img");
+image.src = baconChaddar;
+
+document.body.appendChild(image);
 
 console.log(greeting);
